eb1605cde8799cdef19df15f239bb1f965f20859
Gabriela Guaman <gabrielaguaman@Gabrielas-MacBook-Pro.local>
2023-02-24 14:03:09 -0500

    feat: added sidebar menu

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { AppRoutingModule } from '../app-routing.module';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -10,7 +12,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
   ],
   exports: [SidebarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
